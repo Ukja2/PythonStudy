adbb816c1076fcf6b9ce0ff2bfedff108785a94f
Ukja2 <jaewooklee739@gmail.com>
2024-11-03 15:43:20 +0900

    원의 넓이 구하기

---
+++ Code_step_by_step/Lab5/circle_area.py
@@ -0,0 +1,9 @@
+
+# 원의 반지름을 (radius) 입력받아 해당 반지름을 가진 원의 넓이를 계산하는 함수를 작성하시오
+# 반환 값은 12.566370614359172이 나와야 한다.
+
+
+def circle_area(radius):
+    return(radius ** 2) * 3.141592 #원의 넓이 (radius) * (radius) * fi
+
+print(circle_area(2.0))
