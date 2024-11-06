6be3562ddfc4a278dfbfa65808b3ad3a58239de0
Ukja2 <jaewooklee739@gmail.com>
2024-11-07 02:20:10 +0900

    bmi 프로그램

--- Code_step_by_step/Lab6/bmi.py
+++ Code_step_by_step/Lab6/bmi.py
@@ -28,3 +28,33 @@ class 2
 
 Have a nice day!
 """
+
+def bmi(weight, height):
+    return round(weight / (height**2) * 703, 1)
+
+def category(BMI):
+    if BMI < 18.5:
+        return "class 1"
+    elif 18.5<=BMI <= 24.9:
+        return "class 2"
+    elif 25.0 <= BMI <= 29.9:
+        return "class 3"
+    else:
+        return "class 4"
+
+def main():
+    print("This program reads data for two people")
+    print("and computes their body mass index (BMI).")
+    print()
+    for i in range(1, 3):
+        print("Person", i , "information:")
+        height = float(input("height (in inches)? "))
+        weight = float(input("weight (in pounds)? "))
+        sum = bmi(weight, height)
+        print("BMI =", sum)
+        print(category(sum))
+        print()
+
+
+main()
+print("Have a nice day!")
