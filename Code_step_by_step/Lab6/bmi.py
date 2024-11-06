26143bf88fc56ba849a0ec482c396f0866db707f
Ukja2 <jaewooklee739@gmail.com>
2024-11-07 02:19:40 +0900

    bmi 프로그램

---
+++ Code_step_by_step/Lab6/bmi.py
@@ -0,0 +1,30 @@
+"""
+bmi를 측정하는 프로그램을 작성하시오
+
+bmi 계산 조건 : BMI = weight / height2 * 703
+
+bmi 분류
+BMI	Category
+below 18.5	class 1
+18.5 - 24.9	class 2
+25.0 - 29.9	class 3
+30.0 and up	class 4
+
+두 명의 데이터를 입력받아야 함 (아래는 프로그램 실행시 화면)
+This program reads data for two people
+and computes their body mass index (BMI).
+
+Person 1 information:
+height (in inches)? 70.0
+weight (in pounds)? 194.25
+BMI = 27.9
+class 3
+
+Person 2 information:
+height (in inches)? 62.5
+weight (in pounds)? 130.5
+BMI = 23.5
+class 2
+
+Have a nice day!
+"""
