b07f11f654a52a88f5d688f79acf2b187c05e312
Ukja2 <jaewooklee739@gmail.com>
2024-11-07 01:35:25 +0900

    dracula

---
+++ Code_step_by_step/Lab6/dracula.py
@@ -0,0 +1,49 @@
+"""
+아래 코드를 실행했을 때 나오는 출력값은?
+
+s = "dracula"
+for i in range(len(s)):
+    a = s[0]
+    b = s[1:]
+    s = b + a
+
+"""
+
+"""
+선택지 
+
+1. "dracula"
+
+2. "draculaalucard"
+
+3. "ddrraaccuullaa"
+
+4. "alucarddracula"
+
+5. "alucard"
+
+
+raculad
+
+
+aculadr
+
+
+culadra
+
+
+uladrac
+
+
+ladracu
+
+
+adracul
+
+dracula
+
+정답은 1.
+"""
+
+
+
