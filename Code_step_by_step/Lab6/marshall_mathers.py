26961f8583ebf9ce0f838db1bf288a7fe418df61
Ukja2 <jaewooklee739@gmail.com>
2024-11-07 01:59:49 +0900

    출력값 구하기

---
+++ Code_step_by_step/Lab6/marshall_mathers.py
@@ -0,0 +1,29 @@
+"""
+아래 변수를 참고해서 질문에 대한 출력값을 적으시오
+s1   = "Snoop Dogg"
+s2   = "Marshall 'Eminem' Mathers"
+s3   = s2[18:]
+book = "Building Python Programs"
+"""
+
+"""
+len(s1) : 10
+
+len(s2) : 25
+
+s1.find("o") : 2
+
+s2.find("x") : -1
+
+s1[6:9] : "Dog"
+
+s2[21:] : "hers"
+
+book[9:14] : "Pytho"
+
+s3.lower() : "mathers"
+
+
+
+
+"""
